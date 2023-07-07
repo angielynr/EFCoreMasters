749f8c15046d5ce2757c62d2815f2b618b46b2c4
Angielyn Rivera <2194059@cognizant.com>
2023-07-07 21:48:34 +0800

    CRUD for Product

--- EFCoreAssignment/Program.cs
+++ EFCoreAssignment/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EFCoreAssignment;
+using EFCoreAssignment.Entities;
 using Microsoft.EntityFrameworkCore;
 
 Console.WriteLine("Hello, World!");
@@ -19,6 +20,81 @@ SingleOrDefault(dbContext);
 LoadingRelatedData_Manual(dbContext);
 LoadingRelatedData_ExplicitLoading(dbContext);
 LoadingRelatedData_EagerLoading(dbContext);
+
+InsertProduct(dbContext);
+InsertProductWithNewShop(dbContext);
+UpdateProduct(dbContext);
+DeleteProduct(dbContext);
+DeleteProductByKey(dbContext);
+
+static void InsertProduct(AppDbContext dbContext)
+{
+    var product = new Product
+    {
+        Name = "McFlurry Oreo",
+        ShopId = 3
+    };
+
+    dbContext.Products.Add(product);
+    dbContext.SaveChanges();
+}
+
+static void InsertProductWithNewShop(AppDbContext dbContext)
+{
+    var shop = new Shop
+    {
+        Name = "Coco"
+    };
+
+    var product = new Product
+    {
+        Name = "Ice Water",
+        Shop = shop
+    };
+
+    dbContext.Shops.Add(shop);
+    dbContext.Products.Add(product);
+    dbContext.SaveChanges();
+}
+
+static void UpdateProduct(AppDbContext dbContext)
+{
+    var productId = 3;
+
+    var product = dbContext.Products.SingleOrDefault(p => p.Id == productId);
+    if (product != null)
+    {
+        product.Name = "Dog Food";
+        product.ShopId = 3;
+    }
+
+    dbContext.SaveChanges();
+}
+
+static void DeleteProduct(AppDbContext dbContext)
+{
+    var product = new Product
+    {
+        Id = 8,
+        Name = "Lechon",
+        ShopId = 2
+    };
+
+    dbContext.Products.Remove(product);
+    dbContext.SaveChanges();
+}
+
+static void DeleteProductByKey(AppDbContext dbContext)
+{
+    var productId = 14;
+
+    var product = dbContext.Products.SingleOrDefault(x => x.Id == productId);
+    if (product != null)
+    {
+        dbContext.Products.Remove(product);
+        dbContext.SaveChanges();
+    }
+}
 
 static void Filtering(AppDbContext dbContext)
 {
