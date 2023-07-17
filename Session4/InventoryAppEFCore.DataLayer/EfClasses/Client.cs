ecc51be0eca4e7ff8cd11eed4c5bf75f397ed792
Angielyn Rivera <2194059@cognizant.com>
2023-07-17 19:40:45 +0800

    Uploaded base code

---
+++ Session4/InventoryAppEFCore.DataLayer/EfClasses/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace InventoryAppEFCore.DataLayer.EfClasses
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string Name { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+    }
+}
